0710f3ebd2d7098b990cc1e1019d51137c435d4a
Luis Zaman <luis.zaman@gmail.com>
2021-08-09 14:15:27 -0400

    oops

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+
+import "assets/scss/material-kit-react.scss?v=1.9.0";
+
+// pages for this product
+// import Components from "views/Components/Components.js";
+// import ProfilePage from "views/ProfilePage/ProfilePage.js";
+// import LoginPage from "views/LoginPage/LoginPage.js";
+import LandingPage from "views/LandingPage/LandingPage.js";
+import Module1 from "views/Module1/Module1.js";
+import Module2 from "views/Module2/Module2.js"
+import Module3 from "views/Module3/Module3.js"
+import AboutUs from "views/AboutUs/AboutUs.js"
+
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/module1" >
+          <Module1 />
+        </Route>
+
+        <Route exact path="/module2">
+          <Module2 />
+        </Route>
+
+        <Route exact path="/module3"> 
+          <Module3 />
+        </Route>
+
+        <Route exact path="/aboutus"> 
+          <AboutUs />
+        </Route>
+        <Route exact path="/"> 
+          <LandingPage />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+export default App;
